9efcbbf7f3663ec91e793f035c74c0cb116a54f0
ElfishPresley <mstaehli@ethz.ch>
2016-09-29 12:09:47 +0200

    created XY File

---
+++ app/src/main/java/com/example/matthustahli/xyplot/XYPlotMainActivity.java
@@ -0,0 +1,13 @@
+package com.example.matthustahli.xyplot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class XYPlotMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_xyplot_main);
+    }
+}
